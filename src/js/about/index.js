228fced9525432239366298ee1458ec06fa3d6da
yuxiangw <yuxian660@163.com>
2022-02-06 16:19:40 +0800

    update html

--- src/js/about/index.js
+++ src/js/about/index.js
@@ -1,7 +1,41 @@
 const vAbout = {
+   data() {
+      return {
+         input: '',
+         output: ''
+      }
+   },
+   methods: {
+      toUppercase() {
+         this.output = UTF8ToString(Module._ToUppercase(allocateUTF8OnStack(this.input)))
+      },
+      clear() {
+         this.input = ''
+         this.output = ''
+      }
+   },
    template: `
-      <div>
-         <p>About Page Placeholder</p>
+      <div class='vhome'>
+         <el-row :gutter="20">
+            <el-col class="grid-content bg-purple" :span="12">
+               <el-input
+                  v-model="input"
+                  :rows="18"
+                  type="textarea"
+                  placeholder="Please input"
+               />
+               <el-button @click="toUppercase">To Uppercase</el-button>
+               <el-button @click="clear">Clear</el-button>
+            </el-col>
+            <el-col class="grid-content bg-purple-dark" :span="12">
+               <el-input
+                  v-model="output"
+                  :rows="18"
+                  type="textarea"
+                  placeholder=""
+               />
+            </el-col>
+         </el-row>
       </div>
    `
 }
